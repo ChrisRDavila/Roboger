89cb49dbb841fa9492380a480f634d751a3b98ef
ChrisRDavila <crossdavila@gmail.com>
2023-03-31 12:13:14 -0700

    refactored code now passes test 4, git push rd main

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,19 @@
 
 function roboger(number) {
   const splitArray = number.split("");
-  const numArray = [1];
   outputArray = [];
   splitArray.forEach(element => {
-    for (let i=0;i < element.length; i++); {
-    (numArray.includes(element)) 
-    outputArray.push("Boop");
-      if (!Number(number)) {
-    return false;
-  } else {
-    return true;
-  }
-  }
-  });
-}
+    if (element === ("1")); 
+    outputArray.push("Beep");
+  })
+    return outputArray; 
+  };
+
+
+
+
+//if (!Number(number)) {
+//  return false;
+
+//else {
+//  return outputArray;
