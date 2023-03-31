3c19b7e813d9cb682585074cdc36023dd053f494
ChrisRDavila <crossdavila@gmail.com>
2023-03-31 10:46:40 -0700

    WIP, still trying to set up test 4

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,16 @@
 //Business Logic
 
 function roboger(number) {
+  let splitArray = number.split("");
   const numArray = [1];
+  outputArray = [];
+  splitArray.forEach(element => {
+    for (let i=0;i<element.length; i++);
+    (splitArray.includes)
     if (!Number(number)) {
     return false;
   } else {
     return true;
-  };
+  }
+  });
 };
