f03ed5dc275177add071cc65bfc12713a3a069b0
ChrisRDavila <crossdavila@gmail.com>
2023-04-02 11:28:22 -0700

    realize original function  not meeting prompt rewuirements, attempt tp rebuild 2nd roboger with for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,25 @@
 //Business Logic
 
-function roboger(number) {
-  const splitArray = number.split("");
-  outputArray = [];
-  splitArray.forEach(element => {
-    if (element === "1") {
-    outputArray.push("Beep");
-    } else if (element === "2") {
-    outputArray.push("Boop"); 
-  } else if (element === "3") {
-    outputArray.push("Won't you be my neighbor?");
-  } else {
-    outputArray.push(element);
-  }
-  });  
-return outputArray; 
+//function roboger(number) {
+//  const splitArray = number.split("");
+//  outputArray = [];
+//  splitArray.forEach(element => {
+//    if (element === "1") {
+//    outputArray.push("Beep");
+//    } else if (element === "2") {
+//    outputArray.push("Boop"); 
+//  } else if (element === "3") {
+//    outputArray.push("Won't you be my neighbor?");
+//  } else {
+//    outputArray.push(element);
+//  }
+//  });  
+//return outputArray; 
+//}
+function roboger2(string) {
+
 }
+
   
 //User Interface Logic
 function handleSubmit(event) {
